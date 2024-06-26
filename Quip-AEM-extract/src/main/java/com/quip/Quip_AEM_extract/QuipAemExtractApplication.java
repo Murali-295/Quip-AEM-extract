cd19a5c173ca7d8d0fc0eea61849830d0635e637
Murali Kolli <163140529+Murali-295@users.noreply.github.com>
2024-06-26 14:34:30 +0530

    Initial Commit

---
+++ Quip-AEM-extract/src/main/java/com/quip/Quip_AEM_extract/QuipAemExtractApplication.java
@@ -0,0 +1,13 @@
+package com.quip.Quip_AEM_extract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipAemExtractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipAemExtractApplication.class, args);
+	}
+
+}
---
+++ Quip-AEM-extract/src/test/java/com/quip/Quip_AEM_extract/QuipAemExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quip.Quip_AEM_extract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuipAemExtractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
